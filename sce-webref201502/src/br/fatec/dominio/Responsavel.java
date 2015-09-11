d38496b71ef9eb8c947b34977abda36dd509e97a
esalmeida1 <prof.edsonalmeida@fatec.sp.gov.br>
2015-09-11 15:06:42 -0300

    inclusao da classe hibernate responsavel dao

--- sce-webref201502/src/br/fatec/dominio/Responsavel.java
+++ sce-webref201502/src/br/fatec/dominio/Responsavel.java
@@ -12,4 +12,40 @@ public class Responsavel {
 	String telefoneResponsavel;
 	String setor;
 	String email;
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	public String getTelefoneResponsavel() {
+		return telefoneResponsavel;
+	}
+	public void setTelefoneResponsavel(String telefoneResponsavel) {
+		this.telefoneResponsavel = telefoneResponsavel;
+	}
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
